0c37e921ea955c9cf7c3b1fc70c1b4af5c239ac0
pixrobi <robertoglaviano@gmail.com>
2022-01-04 15:19:39 +0000

    update project edx

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+/*compute function*/
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+
+    /*validation: if negative zero or negative number show alert, focus principal field and stop*/
+    if(principal <= 0){
+        window.alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    /*set text for result and higlight the numbers*/
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
     
+}
+/*function to update dinamically the value in the span */
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
